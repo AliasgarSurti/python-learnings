8c42a8477bf8f7559090072cc891c1353225621a
Aliasgar <aliasgar.surti@lnttechservices.com>
2018-08-22 19:05:02 +0530

    Added description

--- functions.py
+++ functions.py
@@ -1,3 +1,10 @@
+'''
+The sole purpose of this program is to implement a wrapper to 'input' call which takes input from the user.
+Also this code implements the exception handling by which user comes to know that they have entered integer other than integer.
+
+Author: Aliasgar Surti
+Date: 22 August 2018
+'''
 def accept_int(msg):
 	try:
 		a = int(input(msg))
