377dc155ba8f67711ebf8d0da4edfe2d57aec979
Aliasgar <aliasgar.surti@lnttechservices.com>
2018-08-23 13:18:41 +0530

    Added script to convert number passed in argument to list of strings

---
+++ digit_to_string.py
@@ -0,0 +1,36 @@
+'''
+This program's intention is to convert the digit passed in the argument to the string.
+Each digit is stored in array and then it is printed at last.
+e.g. python3.6 digit_to_string.py 56
+output:['five','six']
+
+Author: Aliasgar Surti
+Date: 23 August 2013
+'''
+import sys
+
+numberInStrings = ['zero','one','two','three','four','five','six','seven','eight','nine']
+
+def numberToString(number):
+	try:
+		return numberInStrings[int(number)]
+	except ValueError as err:
+		print (err)
+		exit()
+
+try:
+	digit = sys.argv[1]
+	listOfDigits = []
+	lengthOfDigit = len(digit)
+	param = 0
+	digitInInt = int(digit) # Only to check whether an int is passed.
+	while lengthOfDigit:
+		#print (numberToString(digit[param])) # To print digit individually
+		list.append(listOfDigits,numberToString(digit[param])) # To print digits in array (list)
+		param += 1
+		lengthOfDigit -= 1
+	print (listOfDigits)
+
+except ValueError as err:
+	print (err)
+	exit()
